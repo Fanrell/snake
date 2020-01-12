9c945b0549ca3dfe60d92705705c8200b46b9dbe
Fanrell <cieslik.kamil@outlook.com>
2020-01-12 15:47:25 +0100

    collision, food generation, draw map,struct sanke (not complited)

--- tester.c
+++ tester.c
@@ -1,10 +1,92 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ncurses.h>
+#include <string.h>
+#include <time.h>
+#define ROZMIAR 30
+
+typedef struct snake
+{
+  int posx;
+  int posy;
+  struct snake *next;
+}Snake;
+
+
+
+int food_x = -1024;
+int food_y = -1024;
+int score = 0;
+bool quit = false;
+WINDOW * mainwin;
+
+void map()
+{
+  for(int i=0;i<=ROZMIAR;i++)
+  {
+    mvaddstr(i,0,"|");
+    mvaddstr(i,ROZMIAR*2,"|");
+  }
+  for(int i=0;i<=ROZMIAR*2;i++)
+  {
+    mvaddstr(0,i,"-");
+    mvaddstr(ROZMIAR,i,"-");
+  }
+}
+
+void food()
+{
+  int seed;
+  time_t tt;
+  seed = time(&tt);
+  if(food_x == -1024 && food_y == -1024)
+  {
+    srand(seed);
+    food_x = rand()%ROZMIAR;
+    if(food_x == 0) food_x+=1;
+    if(food_x == 0) food_x+=1;
+    srand(seed);
+    food_y = rand()%(ROZMIAR*2);
+    if(food_y == 0) food_y+=1;
+    if(food_y == (ROZMIAR*2)) food_y-=1;
+  }
+  mvaddstr(food_x,food_y,"*");
+}
+
+void collision(const int x,const int y)
+{
+  if(x == food_x && y == food_y)
+  {
+      score+=10;
+      food_x = -1024;
+      food_y = -1024;
+  }
+  else if(x < 1 || x > ROZMIAR || y < 1 || y > (ROZMIAR*2))
+  {
+    quit = true;
+  }
+}
+
+void snake_move(Snake *wunsz,int x, int y)
+{
+  int o_x, o_y;
+  Snake *iter = wunsz->next;
+  mvaddstr(wunsz->posx, wunsz->posy, "0");
+  collision(wunsz->posx,wunsz->posy);
+  o_x = wunsz->posx;
+  o_y = wunsz->posy;
+  // while (iter != NULL)
+  // {
+  //   iter->posx 
+  // }
+  
+  wunsz->posx += x;
+	wunsz->posy += y;
+
+}
 
 int main(void) 
 {
-  WINDOW * mainwin;
 
   // Initializacja ncurses
   if ( (mainwin = initscr()) == NULL ) {
@@ -22,9 +104,9 @@ int main(void)
   int x,y;
   x = 0;
   y = 1;
-  int posx = 10;
-  int posy = 0;
-  bool quit = false;
+  Snake wunsz;
+  wunsz.posx = 10;
+  wunsz.posy = 10;
   do {
     int c = getch();
     switch (c) 
@@ -55,11 +137,14 @@ int main(void)
         break;
     }
 
-    clear();
-    mvaddstr(posx, posy, "Tu pelznie waz (nacisnij 'q' by zakonczyc)");
-    posx += x;
-	posy += y;
+  clear();
+  map();
+  food();
+  snake_move(&wunsz,x,y);
+  mvaddstr(31,0,"score:");
+  mvaddstr(32,0,"Press q to exit"); 
 	refresh();
+  
   } while( ! quit );
 
 
@@ -69,5 +154,6 @@ int main(void)
   refresh();
   delwin(mainwin);
   endwin();
+  printf("GAME OVER\nScore: %d\n",score);
   return EXIT_SUCCESS;
 }
